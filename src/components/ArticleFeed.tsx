27b012dc796b39d341ddc119dafc62fd56e1e889
Azam Gulov <gulovazam@gmail.com>
2024-02-29 13:05:14 +0500

    Use custom hook to fetch articles
    remove mock data

--- src/components/ArticleFeed.tsx
+++ src/components/ArticleFeed.tsx
@@ -1,46 +1,44 @@
-type ArticleProps = {
-  article: {
-    cover: string;
-    title: string;
-    author: string;
-    date: string;
-  };
-};
-
-const mockArticles = [
-  {
-    cover:
-      "https://ramenparados.com/wp-content/uploads/2020/03/blue-period-horizontal.jpg",
-    title: "How Blue Period conveys art and discovering passion in life",
-    author: "Tsubasa Yamaguchi",
-    date: "2/27/2024",
-  },
-  {
-    cover: "https://img.manga2you.de/2022/04/Berserk.jpg",
-    title:
-      "The struggle of life and how to become a better person conveyed by Berserk",
-    author: "Kentarou Miura",
-    date: "2/27/2024",
-  },
-];
+import ArticlesFetch from "./ArticlesFetch";
+import { Link } from "react-router-dom";
+import { ArticleProps } from "../types/ArticleType";
 
 export default function ArticleFeed() {
+  const { data, error, loading } = ArticlesFetch();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
   return (
     <div className="article-feed">
-      {mockArticles.map((article, index) => (
-        <Article key={index} article={article} />
+      {data.map((article) => (
+        <Article key={article._id} article={article} />
       ))}
     </div>
   );
 }
 
-function Article({ article }: ArticleProps) {
+type Article = {
+  article: ArticleProps;
+};
+
+function Article({ article }: Article) {
   return (
     <article className="article">
-      <img src={article.cover} alt="" className="article__cover" />
-      <h2 className="article__title">{article.title}</h2>
-      <div className="article__author-wrapper">
-        <span className="article__author">{article.author} - </span>
+      <img
+        src={article.cover}
+        alt={`${article.title} cover`}
+        className="article__cover"
+      />
+      <Link to={`/articles/${article._id}`}>
+        <h2 className="article__title">{article.title}</h2>
+      </Link>
+      <div className="article__info">
+        <span className="article__author">{article.author.username} - </span>
         <span className="article__date">{article.date}</span>
       </div>
     </article>
