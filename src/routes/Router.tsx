d30b69b89c2e12d05f395da8fe4c00d3945517cc
Azam Gulov <gulovazam@gmail.com>
2024-02-29 13:06:42 +0500

    Add dynamic articles path

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Root";
+import Home from "./Home";
+import ArticlePage from "../components/ArticlePage";
+import ArticleLoader from "../components/ArticleLoader";
 
 export default function Router() {
   const router = createBrowserRouter([
-    { path: "/", element: <Root />, children: [] },
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "articles/:id",
+          element: <ArticlePage />,
+          loader: ({ params }) => {
+            return ArticleLoader(params.id);
+          },
+        },
+      ],
+    },
   ]);
 
   return <RouterProvider router={router} />;
