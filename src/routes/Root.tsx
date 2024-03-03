ae1c2a94d9d41c3805709e75f8e191d2a0e0b0d9
Azam Gulov <gulovazam@gmail.com>
2024-03-03 18:44:28 +0500

    Add user state
    add user state type
    add outletcontext type
    add useEffect to attemp to login user automatically if cookies are present

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import Nav from "../components/Nav";
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
+import { UserType, SetUserType } from "../types/UserType";
+
+type RootContext = {
+  user: UserType | null;
+} & SetUserType;
 
 export default function Root() {
+  const [user, setUser] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    async function autoLogin() {
+      try {
+        const res = await fetch("http://localhost:3000/auto-login", {
+          method: "POST",
+          mode: "cors",
+          credentials: "include",
+        });
+
+        if (res.ok) {
+          const resData: UserType = await res.json();
+          setUser(resData);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    autoLogin();
+  }, []);
+
   return (
     <>
-      <Nav user={null} />
-      <Outlet />
+      <Nav user={user} setUser={setUser} />
+      <Outlet context={{ user, setUser }} />
     </>
   );
+}
+
+export function UseUser() {
+  return useOutletContext<RootContext>();
 }
