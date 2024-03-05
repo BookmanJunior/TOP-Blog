e0ede22867062e66fcaadc9ee3907f6efec589ec
Azam Gulov <gulovazam@gmail.com>
2024-03-05 16:07:22 +0500

    Refactor Article component
    add FeaturedArticle

--- src/components/ArticleFeed.tsx
+++ src/components/ArticleFeed.tsx
@@ -1,9 +1,13 @@
 import ArticlesFetch from "./ArticlesFetch";
 import { Link } from "react-router-dom";
 import { ArticleProps } from "../types/ArticleType";
+import FeaturedArticle from "./FeaturedArticle";
 import { format } from "date-fns";
+import "../styles/ArticleFeed.scss";
+
 export default function ArticleFeed() {
   const { data, error, loading } = ArticlesFetch();
+  const featuredArticle = data.filter((article) => article.featured)[0];
 
   if (loading) {
     return <div>Loading...</div>;
@@ -14,32 +18,34 @@ export default function ArticleFeed() {
   }
 
   return (
-    <div className="article-feed">
-      {data.map((article) => (
-        <Article key={article._id} article={article} />
-      ))}
-    </div>
+    <>
+      <FeaturedArticle article={featuredArticle} />
+      <div className="article-feed">
+        {data.map((article) => (
+          <Article key={article._id} article={article} />
+        ))}
+      </div>
+    </>
   );
 }
 
-type Article = {
-  article: ArticleProps;
-};
-
-function Article({ article }: Article) {
+function Article({ article }: { article: ArticleProps }) {
   return (
     <article className="article">
       <img
         src={article.cover}
         alt={`${article.title} cover`}
         className="article__cover"
+        loading="lazy"
       />
-      <Link to={`/articles/${article._id}`}>
-        <h2 className="article__title">{article.title}</h2>
-      </Link>
       <div className="article__info">
-        <span className="article__author">{article.author.username} - </span>
-        <span className="article__date">{format(article.date, "PPP")}</span>
+        <Link to={`/articles/${article._id}`}>
+          <h2 className="article__title">{article.title}</h2>
+        </Link>
+        <div>
+          <span className="article__author">{article.author.username} - </span>
+          <span className="article__date">{format(article.date, "PPP")}</span>
+        </div>
       </div>
     </article>
   );
