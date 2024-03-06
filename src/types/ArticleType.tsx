ab6fc4dda26ba25b5cac97d732c104865d05c15e
Azam Gulov <gulovazam@gmail.com>
2024-03-06 18:03:47 +0500

    Add CommentType
    extend ArticleType with CommentType

--- src/types/ArticleType.tsx
+++ src/types/ArticleType.tsx
@@ -1,9 +1,17 @@
+export type commentType = {
+  user: { username: string };
+  text: string;
+  date: string;
+  _id: string;
+};
+
 export type ArticleProps = {
   cover: string;
   title: string;
   author: {
     username: string;
   };
+  comments: commentType[];
   content: string;
   date: string;
   featured: boolean;
