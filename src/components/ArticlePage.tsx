060f6ea259b1cc931d485b51e4ef9ca1a6507b0d
Azam Gulov <gulovazam@gmail.com>
2024-03-06 18:12:34 +0500

    Add CommentForm and Comment components to ArticlePage

--- src/components/ArticlePage.tsx
+++ src/components/ArticlePage.tsx
@@ -1,13 +1,24 @@
 import { useLoaderData } from "react-router-dom";
 import { ArticleProps } from "../types/ArticleType";
+import Comment from "./Comment";
+import CommentForm from "./CommentForm";
+import "../styles/ArticlePage.scss";
 
 export default function ArticlePage() {
   const article = useLoaderData() as ArticleProps;
   return (
     <div className="article-page">
-      <img src={article.cover} />
-      <h2>{article.title}</h2>
-      <div>{article.content}</div>
+      <header className="article-header">
+        <img src={article.cover} />
+        <h1>{article.title}</h1>
+      </header>
+      <div className="article-body">{article.content}</div>
+      <div className="article-comments">
+        <CommentForm articleId={article._id} />
+        {article.comments.map((comment) => (
+          <Comment key={comment._id} comment={comment} />
+        ))}
+      </div>
     </div>
   );
 }
