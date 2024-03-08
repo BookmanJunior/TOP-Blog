a854a1773143bd862cf090be03aaebf2e47fe62d
Azam Gulov <gulovazam@gmail.com>
2024-03-08 15:55:00 +0500

    Use Markdown to render article content

--- src/components/ArticlePage.tsx
+++ src/components/ArticlePage.tsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router-dom";
 import { ArticleProps } from "../types/ArticleType";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
+import Markdown from "react-markdown";
 import "../styles/ArticlePage.scss";
 
 export default function ArticlePage() {
@@ -12,7 +13,15 @@ export default function ArticlePage() {
         <img src={article.cover} />
         <h1>{article.title}</h1>
       </header>
-      <div className="article-body">{article.content}</div>
+      <div className="article-body">
+        <Markdown
+          components={{
+            h1: "h2",
+          }}
+        >
+          {article.content}
+        </Markdown>
+      </div>
       <div className="article-comments">
         <CommentForm articleId={article._id} />
         {article.comments.map((comment) => (
