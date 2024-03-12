30aef5d272e37c8f3dc2cf7edfaf1aa5f1698ecf
Azam Gulov <gulovazam@gmail.com>
2024-03-12 21:01:35 +0500

    Replace ArticleLoader with a custom fetch hook

--- src/components/ArticleLoader.tsx
+++ src/components/ArticleLoader.tsx
@@ -1,21 +1,39 @@
-export default async function ArticleLoader(articleId: string | undefined) {
-  const apiEndPoint = `http://localhost:3000/articles/${articleId}`;
-  try {
-    const res = await fetch(apiEndPoint, {
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-    });
+import { useEffect, useState } from "react";
+import { ArticleProps } from "../types/ArticleType";
+import { useParams } from "react-router-dom";
 
-    if (res.status >= 400) {
-      const { message } = await res.json();
-      throw new Error(message);
+export default function ArticleLoader() {
+  const [article, setArticle] = useState<ArticleProps>({} as ArticleProps);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const apiEndPoint = `http://localhost:3000/articles/${id}`;
+    async function fetchData() {
+      try {
+        const res = await fetch(apiEndPoint, {
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (res.status >= 400) {
+          const { message } = await res.json();
+          throw new Error(message);
+        }
+
+        const resData: ArticleProps = await res.json();
+        setArticle(resData);
+      } catch (error) {
+        throw new Error((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
     }
 
-    const resData = await res.json();
-    return resData;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
+    fetchData();
+  }, [id]);
+
+  return { article, setArticle, loading };
 }
