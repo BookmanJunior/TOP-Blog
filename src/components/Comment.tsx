e9ac3675f1e85c2677feb2ac9300cef9f84babcd
Azam Gulov <gulovazam@gmail.com>
2024-03-13 22:04:04 +0500

    Code refactor

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -11,18 +11,14 @@ export default function Comment({ comment }: { comment: CommentProps }) {
   const isOpen = commentState === 1;
 
   return (
-    <div className={`comment`}>
-      <button onClick={() => setCommentState(isOpen ? 0 : 1)}>
-        <span>+</span>
-      </button>
-      <div className="comment-wrapper">
-        <div className="comment-header">
-          <p className="comment-author">{comment.user.username}</p>
-          <span className="comment-date">{format(comment.date, "MMM d")}</span>
-        </div>
-        <div className="comment-body" data-expanded={`${isOpen}`}>
-          <div>{comment.text}</div>
-        </div>
+    <div className="comment">
+      <div className="comment-header">
+        <p className="comment-author">{comment.user.username}</p>
+        <span className="comment-date">{format(comment.date, "MMM d")}</span>
+        <button onClick={() => setCommentState(isOpen ? 0 : 1)}></button>
+      </div>
+      <div className="comment-body" data-expanded={`${isOpen}`}>
+        <div>{comment.text}</div>
       </div>
     </div>
   );
