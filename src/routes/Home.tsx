b2fff9eb1339d4b5b004b61d7012b6478170c9b3
Azam Gulov <gulovazam@gmail.com>
2024-03-13 18:11:08 +0500

    Add FeaturedArticle component
    import custom hook to fetch articles

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,27 @@
 import ArticleFeed from "../components/ArticleFeed";
+import FeaturedArticle from "../components/FeaturedArticle";
+import ArticlesFetch from "../components/ArticlesFetch";
 
 export default function Home() {
-  return <ArticleFeed />;
+  const { data, error, loading } = ArticlesFetch();
+  const featuredArticle = data.filter((article) => article.featured)[0];
+
+  if (loading) {
+    return <div className="spinner"></div>;
+  }
+
+  if (error) {
+    return (
+      <div className="error">
+        Something went wrong. Please refresh the page.
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <FeaturedArticle article={featuredArticle} />
+      <ArticleFeed articles={data} />
+    </>
+  );
 }
