cf2680f3a83eddf7514256d7ac34b1719b37010b
Azam Gulov <gulovazam@gmail.com>
2024-04-01 20:49:00 +0500

    Return different component on conditional deleted check

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,16 +1,24 @@
 import { format } from "date-fns";
-import { commentType } from "../types/ArticleType";
 import { useState } from "react";
 import "../styles/Comment.scss";
+import { CommentType } from "../types/CommentType";
 
-export default function Comment({ comment }: { comment: commentType }) {
+export default function Comment({ comment }: { comment: CommentType }) {
   const [commentState, setCommentState] = useState(1);
   const isOpen = commentState === 1;
+
+  if (comment.deleted) {
+    return (
+      <div className="comment">
+        <p className="comment-author">Deleted</p>
+      </div>
+    );
+  }
 
   return (
     <div className="comment">
       <div className="comment-header">
-        <p className="comment-author">{comment.user.username}</p>
+        <p className="comment-author">{comment.user?.username}</p>
         <span className="comment-date">{format(comment.date, "MMM d")}</span>
         <button onClick={() => setCommentState(isOpen ? 0 : 1)}></button>
       </div>
