6bc0b3a2bb108685080ffb9c3a0b88d6082d1ab8
Azam Gulov <gulovazam@gmail.com>
2024-04-08 17:24:13 +0500

    Add FeaturedArticle type
    use optional chaining for article properties

--- src/components/Article/FeaturedArticle.tsx
+++ src/components/Article/FeaturedArticle.tsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 import { ArticleProps } from "../../types/ArticleType";
 import "../../styles/FeaturedArticle.scss";
 
-export default function FeaturedArticle({
-  article,
-}: {
-  article: ArticleProps;
-}) {
+type FeaturedArticleProps = {
+  article: ArticleProps | undefined;
+};
+
+export default function FeaturedArticle({ article }: FeaturedArticleProps) {
   return (
-    <Link to={`articles/${article._id}`} className="featured-article">
+    <Link to={`articles/${article?._id}`} className="featured-article">
       <div>
         <span className="featured-tag">Featured Article</span>
-        <h1>{article.title}</h1>
+        <h1>{article?.title}</h1>
       </div>
-      <img src={article.cover} alt={"Featured article cover"} loading="lazy" />
+      <img src={article?.cover} alt={"Featured article cover"} loading="lazy" />
     </Link>
   );
 }
