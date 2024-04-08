df8773b0e304415392d0b40d14452d65b5f95c82
Azam Gulov <gulovazam@gmail.com>
2024-04-08 17:20:52 +0500

    Replace ArticlesFetch hook with DataFetch
    check for data before rending ArticlePreview

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,28 +1,27 @@
 import ArticlePreview from "../components/Article/ArticlePreview";
 import FeaturedArticle from "../components/Article/FeaturedArticle";
-import ArticlesFetch from "../components/ArticlesFetch";
+import DataFetch from "../components/DataFetch";
+import { ArticleProps } from "../types/ArticleType";
 
 export default function Home() {
-  const { data, error, loading } = ArticlesFetch();
-  const featuredArticle = data.filter((article) => article.featured)[0];
+  const { data, error, loading } = DataFetch<ArticleProps[]>(
+    "http://localhost:3000/articles"
+  );
+  const featuredArticle = data?.filter((article) => article.featured)[0];
 
   if (loading) {
     return <div className="spinner"></div>;
   }
 
   if (error) {
-    return (
-      <div className="error">
-        Something went wrong. Please refresh the page.
-      </div>
-    );
+    throw error;
   }
 
   return (
     <>
       <FeaturedArticle article={featuredArticle} />
       <section className="article-feed">
-        {data.length ? (
+        {data ? (
           data.map((article) => (
             <ArticlePreview key={article._id} article={article} />
           ))
