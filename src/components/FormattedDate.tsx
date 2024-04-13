40fb3218f41d3be3776dd634301d239490eb0678
Azam Gulov <gulovazam@gmail.com>
2024-04-13 17:44:28 +0500

    Add optional isTime property

--- src/components/FormattedDate.tsx
+++ src/components/FormattedDate.tsx
@@ -3,8 +3,17 @@ import { format } from "date-fns";
 type FormattedDateProps = {
   date: string;
   className?: string;
+  isTime?: boolean;
 };
 
-export default function FormattedDate({ date, className }: FormattedDateProps) {
-  return <p className={className}>{format(date, "PPP")}</p>;
+export default function FormattedDate({
+  date,
+  className,
+  isTime = false,
+}: FormattedDateProps) {
+  return (
+    <p className={className}>
+      {isTime ? format(date, "HH:mm - PPP") : format(date, "PPP")}
+    </p>
+  );
 }
