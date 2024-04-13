6df2688aef2220ea52f8789dffb1ea359b1d0281
Azam Gulov <gulovazam@gmail.com>
2024-04-13 18:29:28 +0500

    Add delete button
    add DeleteButtonProps
    add CommentProps

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -2,8 +2,22 @@ import { useState } from "react";
 import { CommentType } from "../../types/CommentType";
 import FormattedDate from "../FormattedDate";
 import "../../styles/Comment.scss";
+import { UseUser } from "../../routes/Root";
 
-export default function Comment({ comment }: { comment: CommentType }) {
+type handleDelete = (commentId: string) => void;
+
+type CommentProps = {
+  comment: CommentType;
+  handleDelete: handleDelete;
+};
+
+type DeleteButtonProps = {
+  commentId: string;
+  handleDelete: handleDelete;
+};
+
+export default function Comment({ comment, handleDelete }: CommentProps) {
+  const { user } = UseUser();
   const [commentState, setCommentState] = useState(1);
   const isOpen = commentState === 1;
 
@@ -15,16 +29,57 @@ export default function Comment({ comment }: { comment: CommentType }) {
     );
   }
 
+  const deleteButton = (
+    <CommentDeleteButton commentId={comment._id} handleDelete={handleDelete} />
+  );
+
   return (
     <div className="comment">
       <div className="comment-header">
         <p className="comment-author">{comment.user?.username}</p>
-        <FormattedDate className="comment-date" date={comment.date} />
-        <button onClick={() => setCommentState(isOpen ? 0 : 1)}></button>
+        <FormattedDate
+          className="comment-date"
+          isTime={true}
+          date={comment.date}
+        />
+        <button
+          className="expand-button"
+          onClick={() => setCommentState(isOpen ? 0 : 1)}
+        ></button>
       </div>
       <div className="comment-body" data-expanded={`${isOpen}`}>
-        <div>{comment.text}</div>
+        <div>
+          {comment.text}
+          {(user?._id === comment.user?._id && deleteButton) ||
+            (user?.role === "admin" && deleteButton)}
+        </div>
       </div>
     </div>
   );
+}
+
+function CommentDeleteButton({ commentId, handleDelete }: DeleteButtonProps) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <button>Delete Comment</button>
+    </form>
+  );
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/comments/${commentId}`, {
+        method: "DELETE",
+        mode: "cors",
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        handleDelete(commentId);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
