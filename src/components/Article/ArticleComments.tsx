00aee171d3e9597dd4a6e2570d1ce7dd72d8b861
Azam Gulov <gulovazam@gmail.com>
2024-04-13 18:49:14 +0500

    Add deleteComment function

--- src/components/Article/ArticleComments.tsx
+++ src/components/Article/ArticleComments.tsx
@@ -3,6 +3,7 @@ import { ArticleProps } from "../../types/ArticleType";
 import Comment from "../Comment/Comment";
 import CommentForm from "../Comment/CommentForm";
 import { UserType } from "../../types/UserType";
+import { CommentType } from "../../types/CommentType";
 
 type CommentSectionProps = {
   article: ArticleProps;
@@ -23,11 +24,27 @@ export default function ArticleComments({
         <UnAuthorizedLinks />
       )}
       {article.comments.map((comment) => (
-        <Comment key={comment._id} comment={comment} />
+        <Comment
+          key={comment._id}
+          comment={comment}
+          handleDelete={deleteComment}
+        />
       ))}
     </section>
   );
+
+  function deleteComment(commentId: string) {
+    const updatedComments = article?.comments.map((comment): CommentType => {
+      if (comment._id === commentId) {
+        comment.deleted = true;
+        return comment;
+      }
+      return comment;
+    });
+    setArticle({ ...article, comments: updatedComments });
+  }
 }
+
 function UnAuthorizedLinks() {
   const location = useLocation();
   return (
